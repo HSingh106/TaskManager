05473a72c9c74f3cef9b25f08590c72b10731804
HSingh106 <154386123+HSingh106@users.noreply.github.com>
2024-07-20 19:44:41 -0400

    modified tests in task repository to line up with unidirectional flow change, will be adding remainder of essential tests in

--- src/test/java/com/example/TaskManager/Repository/TaskRepositoryIntegrationTests.java
+++ src/test/java/com/example/TaskManager/Repository/TaskRepositoryIntegrationTests.java
@@ -3,6 +3,7 @@ package com.example.TaskManager.Repository;
 import com.example.TaskManager.Model.Task;
 import com.example.TaskManager.Model.User;
 import com.example.TaskManager.TestDataUtil;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,22 +36,28 @@ public class TaskRepositoryIntegrationTests {
         User user = TestDataUtil.createTestUserTwo();
         userRepository.save(user);
         Task task = TestDataUtil.createTestTaskOne();
-        taskRepository.save(task);
-        Optional<Task> taskOptional = taskRepository.findById(task.getId());
-        assertThat(taskOptional).isPresent();
-        assertThat(taskOptional.get()).isEqualTo(task);
+        task.setId(1L);
+        user.getTasks().add(task);
+        userRepository.save(user);
+        Iterable<Task> result = taskRepository.findAll();
+        assertThat(result).hasSize(1).containsExactly(task);
     }
 
     @Test
+    @Transactional
     public void TaskRepository_CreateAndRecallMultipleTasks_ReturnCreatedTasks(){
         User user = TestDataUtil.createTestUserOne();
         userRepository.save(user);
         Task task1 = TestDataUtil.createTestTaskOne();
-        taskRepository.save(task1);
         Task task2 = TestDataUtil.createTestTaskTwo();
-        taskRepository.save(task2);
         Task task3 = TestDataUtil.createTestTaskThree();
-        taskRepository.save(task3);
+        task1.setId(1L);
+        task2.setId(2L);
+        task3.setId(3L);
+        user.getTasks().add(task1);
+        user.getTasks().add(task2);
+        user.getTasks().add(task3);
+        userRepository.save(user);
         Iterable<Task> tasks = taskRepository.findAll();
         assertThat(tasks).hasSize(3).contains(task1,task2, task3);
     }
