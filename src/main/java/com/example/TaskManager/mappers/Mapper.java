1ad83ca7d2e1815c095c1ceba142a6dcc4620b25
HSingh106 <154386123+HSingh106@users.noreply.github.com>
2024-07-20 22:55:34 -0400

    implemented mapping functionality from user to userDTO and reverse

---
+++ src/main/java/com/example/TaskManager/mappers/Mapper.java
@@ -0,0 +1,9 @@
+package com.example.TaskManager.mappers;
+
+public interface Mapper<A, B> {
+
+    B mapTo(A a);
+
+    A mapFrom(B b);
+
+}
---
+++ src/main/java/com/example/TaskManager/mappers/implementation/UserMapper.java
@@ -0,0 +1,28 @@
+package com.example.TaskManager.mappers.implementation;
+
+import com.example.TaskManager.Model.DTO.UserDTO;
+import com.example.TaskManager.Model.Entities.User;
+import com.example.TaskManager.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper implements Mapper<User, UserDTO> {
+
+    private ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public
+    @Override
+    public UserDTO mapTo(User user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    @Override
+    public User mapFrom(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
+    }
+}
