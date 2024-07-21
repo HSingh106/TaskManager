6c0df0d5952f28859affb479ed490e4b2a7f6a70
HSingh106 <154386123+HSingh106@users.noreply.github.com>
2024-07-20 20:35:49 -0400

    finished updating and adding remainder of tests for task repository

--- src/test/java/com/example/TaskManager/Repository/TaskRepositoryIntegrationTests.java
+++ src/test/java/com/example/TaskManager/Repository/TaskRepositoryIntegrationTests.java
@@ -36,11 +36,17 @@ public class TaskRepositoryIntegrationTests {
         User user = TestDataUtil.createTestUserTwo();
         userRepository.save(user);
         Task task = TestDataUtil.createTestTaskOne();
+        Task task2 = TestDataUtil.createTestTaskTwo();
+        task.setId(1L);
+        task2.setId(2L);
         task.setId(1L);
         user.getTasks().add(task);
         userRepository.save(user);
         Iterable<Task> result = taskRepository.findAll();
         assertThat(result).hasSize(1).containsExactly(task);
+        taskRepository.save(task2);
+        result = taskRepository.findAll();
+        assertThat(result).hasSize(2).containsExactly(task,task2);
     }
 
     @Test
@@ -61,5 +67,50 @@ public class TaskRepositoryIntegrationTests {
         Iterable<Task> tasks = taskRepository.findAll();
         assertThat(tasks).hasSize(3).contains(task1,task2, task3);
     }
+
+    @Test
+    public void TaskRepository_TaskCreateAndUpdate_ReturnUpdatedTask(){
+        User user = TestDataUtil.createTestUserOne();
+        userRepository.save(user);
+        Task task = TestDataUtil.createTestTaskOne();
+        task.setId(1L);
+        taskRepository.save(task);
+        user.getTasks().add(task);
+        userRepository.save(user);
+        Iterable<Task> result = taskRepository.findAll();
+        assertThat(result).hasSize(1).containsExactly(task);
+        task.setTaskDescription("Changes");
+        taskRepository.save(task);
+        result = taskRepository.findAll();
+        assertThat(result).hasSize(1).containsExactly(task);
+
+    }
+
+    @Test
+    public void TaskRepository_TaskDelete_ReturnDeletedTask(){
+        User user = TestDataUtil.createTestUserOne();
+        userRepository.save(user);
+        Task task = TestDataUtil.createTestTaskOne();
+        task.setId(1L);
+        taskRepository.save(task);
+        Task task2 = TestDataUtil.createTestTaskTwo();
+        task2.setId(2L);
+        taskRepository.save(task2);
+        Task task3 = TestDataUtil.createTestTaskThree();
+        task3.setId(3L);
+        taskRepository.save(task3);
+        user.getTasks().add(task);
+        user.getTasks().add(task2);
+        user.getTasks().add(task3);
+        userRepository.save(user);
+        taskRepository.delete(task);
+        taskRepository.delete(task2);
+        taskRepository.delete(task3);
+        Iterable<Task> result = taskRepository.findAll();
+        assertThat(result).hasSize(0);
+
+    }
+
+
 
 }
